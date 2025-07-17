4ec960f0a1e44e05962436ffd998674d8fdbc9ed
Simon <njogusimong@gmail.com>
2025-07-17 13:34:35 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header({ onDarkModeClick }) {
+  return (
+    <header>
+      <h1>Shopping List</h1>
+      <button onClick={onDarkModeClick}>Dark Mode</button>
+    </header>
+  );
+}
+
+export default Header;
